af9630842c416cbd802c75d2867141621c246bb6
jiyeong <jiyeong.dev@gmail.com>
2025-05-30 05:38:35 +0900

    add health check API

---
+++ src/main/java/com/sdemo1/controller/HealthCheckController.java
@@ -0,0 +1,21 @@
+package com.sdemo1.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sdemo1.common.response.ApiResponse;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health-check")
+    public ApiResponse<String> healthCheck() {
+        return new ApiResponse<>(
+            true,
+            "서버가 정상적으로 동작중입니다.",
+            "OK",
+            HttpStatus.OK
+        );
+    }
+} 
