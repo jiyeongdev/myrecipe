99fe1c849315353f91f600f53e3e3ca2f8cacb3a
jiyeong <jiyeong.dev@gmail.com>
2025-05-31 01:56:34 +0900

    remove cloudwatch log health-check

--- src/main/java/com/sdemo1/interceptor/LoggingInterceptor.java
+++ src/main/java/com/sdemo1/interceptor/LoggingInterceptor.java
@@ -14,9 +14,14 @@ public class LoggingInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         String requestURI = request.getRequestURI();
+        
+        // /health-check 경로는 로깅하지 않음
+        if (requestURI.equals("/health-check")) {
+            return true;
+        }
+        
         String method = request.getMethod();
         String queryString = request.getQueryString();
-        
         
         String fullUrl = queryString != null ? 
         requestURI + "?" + queryString : 
@@ -29,6 +34,12 @@ public class LoggingInterceptor implements HandlerInterceptor {
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) {
         String requestURI = request.getRequestURI();
+        
+        // /health-check 경로는 로깅하지 않음
+        if (requestURI.equals("/health-check")) {
+            return;
+        }
+        
         String method = request.getMethod();
         int status = response.getStatus();
         
