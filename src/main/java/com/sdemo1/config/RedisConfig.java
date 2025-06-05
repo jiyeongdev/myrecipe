a0044be0d621f160e5e007bff06abab755fae704
jiyeong <jiyeong.dev@gmail.com>
2025-06-06 02:21:43 +0900

    Could not safely identify store assignment for repository candidate interface" 오류

--- src/main/java/com/sdemo1/config/RedisConfig.java
+++ src/main/java/com/sdemo1/config/RedisConfig.java
@@ -3,17 +3,16 @@ package com.sdemo1.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-@EnableRedisRepositories(
-    basePackages = "com.sdemo1.repository.redis"  // Redis 레포지토리만 별도 패키지에 두기
-)
+@EnableJpaRepositories(basePackages = "com.sdemo1.repository") // JPA 레포지토리 패키지 명시
+// Redis 레포지토리를 사용하지 않으므로 @EnableRedisRepositories 제거
 public class RedisConfig {
 
     @Value("${redis.host}")
